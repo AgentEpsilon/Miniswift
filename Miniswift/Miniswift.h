b2bdbad0560ef8cbba2e2741d0f2a18d267ba59f
Evan Miller <miller.evan815@gmail.com>
2015-10-14 21:57:03 -0400

    Initial Commit

---
+++ Miniswift/Miniswift.h
@@ -0,0 +1,19 @@
+//
+//  Miniswift.h
+//  Miniswift
+//
+//  Created by Evan Miller on 10/14/15.
+//  Copyright © 2015 Evan Miller. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Miniswift.
+FOUNDATION_EXPORT double MiniswiftVersionNumber;
+
+//! Project version string for Miniswift.
+FOUNDATION_EXPORT const unsigned char MiniswiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Miniswift/PublicHeader.h>
+
+
